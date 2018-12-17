6e44c77a230ff19f2d8707c089f245a639248236
Kyle Polich <kyle@dataskeptic.com>
2018-12-16 21:08:25 -0800

    podcast page wip

---
+++ src/views/Podcast/PodcastCard/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import moment from 'moment';
+
+import './styles.scss';
+
+import Loading from 'Components/Loading';
+
+class PodcastCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+    };
+  }
+  componentDidMount() {
+    const { post } = this.props;
+    post.related = post.related || [];
+    post.related.map(content => {
+      if (content.type === 'homepage-image') {
+        this.image = new Image();
+        this.image.onload = event => {
+          this.setState({ loaded: true });
+        }
+        this.image.src = content.dest;
+      }
+    });
+    if (!this.image) this.setState({ loaded: true });
+  }
+  handleEvent(event_name, event_data = {}) {
+    const component = this.constructor.name;
+    switch(event_name) {
+      case 'ON_LOAD':
+        return event => {
+          this.setState({ loaded: true });
+        };
+      default:
+        return event => {
+          console.warn(component + '::Event', 'No handler for "' + event_name + '" found.', { event_data, event });
+        };
+    }
+  }
+  render() {
+    const { api, post } = this.props;
+    const { podcasts } = api || {};
+    const { blog_id, title, abstract, publish_date } = post;
+    let date = moment(publish_date, "YYYYMMDD").fromNow();
+    if (date.includes('month')) date = moment(publish_date).format("MMMM Do YYYY");
+    /*
+      date
+      title
+      play | download
+      desc
+      read more
+      guest image
+    */
+    return (
+      <article className="PodcastCard col-xs-12">
+        <div className="podcast">
+          <Loading on={this.state.loaded && podcasts[blog_id]}>
+            <div classname="row">
+              <div className="podcast-image col-xs-3">
+                {this.image && <img className="podcast-img-main" src={this.image.src} alt={title} />}
+              </div>
+              <div className="podcast-body col-xs-9">
+              Test
+                <p className="podcast-date">{date}</p>
+                <h5 className="podcast-title">{title}</h5>
+                <p className="podcast-text">{abstract}</p>
+                <Link to={`/blog/${blog_id}`} className="btn btn-primary">Read More</Link>
+              </div>
+            </div>
+          </Loading>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default connect(state => ({
+  api: state.api,
+  status: state.status
+}))(PodcastCard);
--- src/views/Podcast/index.js
+++ src/views/Podcast/index.js
@@ -5,6 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './styles.scss';
 
+import PodcastCard from './PodcastCard';
+import BlogCard from '../Blog/BlogCard';
+
+import Loading from 'Components/Loading';
+
 import Navbar from 'Components/Navbar';
 import Footer from 'Components/Footer';
 
@@ -25,11 +30,24 @@ class Podcast extends Component {
     }
   }
   render() {
+    const { api, status } = this.props || {};
+    const { podcasts } = api || {};
+    const blogs = podcasts
+    const { blog_id } = this.props.match.params;
+    const sorted_blogs = Object.values(blogs || {}).sort((a, b) => ((new Date(a.publish_date)) < (new Date(b.publish_date)) ? 1 : -1));
     return (
       <React.Fragment>
         <Navbar active={this.constructor.name} />
-        <main className="container">
-          Podcast
+        <main id="Podcast" className="container">
+          <Loading on={status.ready}>
+            {blog_id && (
+              <BlogPost post={blogs[blog_id]} />
+            ) || (
+              <div className="row open-gutter">
+                {sorted_blogs.map((blog, b) => <PodcastCard post={blog} key={b} />)}
+              </div>
+            )}
+          </Loading>
         </main>
         <Footer />
       </React.Fragment>
@@ -38,5 +56,6 @@ class Podcast extends Component {
 }
 
 export default connect(state => ({
+  api: state.api,
   status: state.status
 }))(Podcast);
