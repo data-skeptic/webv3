406485c4608e06749fa7bf3b3ce9177cde6997e0
Kyle Polich <kyle@dataskeptic.com>
2018-12-22 11:24:01 -0800

    stub out homepage player

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
@@ -33,12 +34,23 @@ class Home extends Component {
     const { blogs } = api || {};
     const sorted_blogs = Object.values(blogs || {}).sort((a, b) => ((new Date(a.publish_date)) < (new Date(b.publish_date)) ? 1 : -1));
     const { log } = this.state;
+    const podcast_page = "/blog/episodes/2018/drug-discovery-with-machine-learning"
     var bot_id = "f06d0f04-02a2-560d-9c0c-2220f96ec56f"
     return (
       <main className="Home container">
         <div className="row">
-          <div className="col-xs-12 col-sm-6">
+          <div className="col-xs-12 col-sm-2">
+            <div id="bot-img-container">
+              <img id="bot-img" src="https://s3.amazonaws.com/dataskeptic.com/img/bot/bot-200-200.png" />
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-5">
             <ChatBot bot_id={bot_id} />
+          </div>
+          <div id="homepage-player" className="col-xs-12 col-sm-3">
+            <div className="homepage-player-title"><Link to={podcast_page}>Title</Link></div>
+            <div className="homepage-player-desc">Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </div>
+            <button className="homepage-player-button">Play</button>
           </div>
         </div>
       </main>
