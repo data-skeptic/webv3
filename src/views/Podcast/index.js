bdf2e13832c5af7fe9bbd46ec86995d1ff6c95e1
Cory Armstrong <c0deslave49@gmail.com>
2019-01-07 15:46:01 -0800

    added podcast years display and functionality

--- src/views/Podcast/index.js
+++ src/views/Podcast/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
@@ -26,23 +27,45 @@ class Podcast extends Component {
   render() {
     const { api, status } = this.props || {};
     let { category, year, name } = this.props.match.params;
-    category = category || 'all';
+    console.log(year);
+    category = category || 'episodes';
     const podcasts = api.podcasts || {};
-    const category_podcasts = podcasts[category] || [];
+    const category_podcasts = podcasts[category] || {};
+    const year_podcasts = podcasts[category] && podcasts[category][year] ? podcasts[category][year] : [];
     const post = podcasts[category] && podcasts[category][year] && podcasts[category][year][name] ? podcasts[category][year][name] : undefined;
-    console.log({podcasts, category, year, name, post});
     return (
-      <main id="Podcast" className="container">
-        <Loading on={status.ready}>
-          {post && (
-            <PodcastPost post={post} />
-          ) || (
-            <React.Fragment>
-              {category_podcasts.map((podcast, p) => <PodcastCard post={podcast} key={p} />)}
-            </React.Fragment>
-          )}
-        </Loading>
-      </main>
+      <React.Fragment>
+        <main id="Podcast" className="container">
+          <div className="row">
+            <div className={`col-12${!post ? ' col-sm-9' : ''}`}>
+              <Loading on={status.ready}>
+                {post && (
+                  <PodcastPost post={post} />
+                ) || year && (
+                  <React.Fragment>
+                    {Object.values(year_podcasts).map((podcast, p) => <PodcastCard post={podcast} key={p} />)}
+                  </React.Fragment>
+                ) || (
+                  <React.Fragment>
+                    {Object.keys(category_podcasts).map((year, y) => (
+                      <React.Fragment key={y}>
+                        {Object.values(category_podcasts[year]).map((podcast, p) => <PodcastCard post={podcast} key={p} />)}
+                      </React.Fragment>
+                    ))}
+                  </React.Fragment>
+                )}
+              </Loading>
+            </div>
+            {!post && (
+              <aside className="col-12 col-sm-3 sticky-top">
+                <div className="list-group">
+                  {Object.keys(category_podcasts).map((cat_year, y) => <Link className={`list-group-item list-group-item-action${cat_year === year ? ' active' : ''}`} to={`/podcast/${category}/${cat_year}`} key={y}>{cat_year}</Link>)}
+                </div>
+              </aside>
+            )}
+          </div>
+        </main>
+      </React.Fragment>
     );
   }
 }
